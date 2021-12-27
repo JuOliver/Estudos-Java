dcdd5de8c3d8009294a5bf04776ccb42677abff9
Juliana Oliveira <juoliverpt@gmail.com>
2021-12-27 12:30:22 -0400

    Utilizando Paths.get() para localizar arquivo

---
+++ src/wnio/PathTeste1.java
@@ -0,0 +1,19 @@
+package wnio;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class PathTeste1 {
+    public static void main(String[] args) {
+        Path p1 = Paths.get("C:\\Users\\admin\\Documents\\Estudos-Java\\file.txt");
+        Path p2 = Paths.get("C:\\Users\\admin\\Documents\\Estudos-Java","file.txt");
+        Path p3 = Paths.get("C:","Users\\admin\\Documents\\Estudos-Java", "file.txt");
+        Path p4 = Paths.get("C:","Users","admin","Documents","Estudos-Java","file.txt");
+
+        System.out.println(p1.getFileName());
+        System.out.println(p2.getFileName());
+        System.out.println(p3.getFileName());
+        System.out.println(p4.getFileName());
+    }
+
+}
