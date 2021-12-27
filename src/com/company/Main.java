838f1387e39ee5e36a9ebcef3f9170d1d2cccfce
Juliana <juoliverpt@gmail.com>
2021-12-26 22:56:22 -0400

    Adicionando estudos sobre Path

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import model.Produto;
+
+public class Main {
+
+    public static void main(String[] bolinho) {
+//        Alt + Shift + Setas: Mover linha atual
+//        SyCtrl + Shift + Setas: Mover linha atual mantendo contextostem.out.println("Testando");
+
+
+
+        // deixa so new Produto() => alt + enter em new vai aparecer a opção introduce local variable
+        Produto produto = new Produto("refri", 5.00, 10);
+    }
+}
---
+++ src/model/Produto.java
@@ -0,0 +1,39 @@
+package model;
+
+public class Produto {
+    private String nome;
+    private double valor;
+    private int quantidade;
+
+    public Produto(String nome, double valor, int quantidade) {
+        this.nome = nome;
+        this.valor = valor;
+        this.quantidade = quantidade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+
+}
