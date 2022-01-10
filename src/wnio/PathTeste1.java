0cdc56f545a7b3e7098b6aa7ca0817c369dce725
Juliana <juoliverpt@gmail.com>
2022-01-10 14:47:26 -0400

    Adicionando comentários para ficar mais explicativo

--- src/wnio/PathTeste1.java
+++ src/wnio/PathTeste1.java
@@ -5,6 +5,7 @@ import java.nio.file.Paths;
 
 public class PathTeste1 {
     public static void main(String[] args) {
+//        Formas de adicionar arquivos sem sobrecarga
         Path p1 = Paths.get("C:\\Users\\admin\\Documents\\Estudos-Java\\file.txt");
         Path p2 = Paths.get("C:\\Users\\admin\\Documents\\Estudos-Java","file.txt");
         Path p3 = Paths.get("C:","Users\\admin\\Documents\\Estudos-Java", "file.txt");
